3087072193e8d4042557b4d737435279b6bd00e1
Victori <vika.kampo03@gmail.com>
2021-03-24 11:39:16 +0200

    Init

---
+++ front/src/components/Zavd2.vue
@@ -0,0 +1,8 @@
+<template>
+<div>
+    <h1>Бібліотека Multer</h1>
+    <h3>
+Multer - це проміжне програмне забезпечення для фреймворка express для обробки multipart/form-data, потрібна в першу чергу при завантаженні файлів. Multer і не обробляє жодний ніякий інший тип форм, крім multipart/form-data.
+</h3>
+</div>
+</template>
